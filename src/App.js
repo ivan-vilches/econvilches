782d5ef7e0e7eee5de22de5abccaef18dfefaf31
Ivan Vilches Basaul <ivanvilches@MacBook-Air-de-Ivan.local>
2021-09-06 17:06:15 -0300

    add simple navbar

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import NavBar from "./components/NavBar";
+
+const App = () => {
+  return (
+    <header className="test">
+      <h1> Hello World</h1>
+      <NavBar />
+    </header>
+  );
+};
+
+export default App;
